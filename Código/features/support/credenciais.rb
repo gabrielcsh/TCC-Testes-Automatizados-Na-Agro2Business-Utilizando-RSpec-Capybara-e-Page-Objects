46080348e9330a19fef8ebff2d3bfce157a31295
Ana Paula Ferreira Barroso de Castro <aninhabarroso16.apb@gmail.com>
2023-05-16 10:43:08 -0300

    atualizações feitas devido ao reset do ambiente local

--- Código/features/support/credenciais.rb
+++ Código/features/support/credenciais.rb
@@ -10,7 +10,7 @@ CREDENCIAIS = {
     },
     usuario_cadastro:{
         nome: 'José dos Santos',
-        email: 'santosjj@teste.com',
+        email: 'santoss@teste.com',
         telefone: '(67)99123-4567',
         whatsapp: '(67)9123-4567',
         senha: '741852'
